24d3d3fca7a49bb18f15ccdda32739770a8b81f2
Mesti <timisare@gmail.com>
2025-01-09 00:16:51 +0100

    Initial commit with initial stuff

---
+++ mjscreen.client/src/components/Uploader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="file" accept="audio/*" @change="handleFileChange" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  methods: {
+    handleFileChange(event: Event) {
+      const file = (event.target as HTMLInputElement).files?.[0];
+      if (file) {
+        // Émettre un événement pour signaler qu'un fichier a été sélectionné
+        this.$emit('file-selected', file);
+      }
+    }
+  }
+});
+</script>
+
+<style scoped>
+  /* Ajoute du style si nécessaire */
+</style>
