0e6698fb7dc5c451b1bbf5e8c21c20af26f6da98
Mestileryan <timisare@gmail.com>
2025-01-23 23:55:15 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,68 +1,22 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-vue';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
-import { env } from 'process';
-import svgLoader from 'vite-svg-loader';
-
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
-
-const certificateName = "mjscreen.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(baseFolder)) {
-    fs.mkdirSync(baseFolder, { recursive: true });
-}
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7036';
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [plugin(),
+  plugins: [
+    vue(),
     svgLoader()
   ],
-  base: '/MJScreen/', // Remplace "nom-du-repo" par le nom de ton dépôt GitHub
+  // Ajuste la valeur de `base` selon ton dépôt / URL de déploiement.
+  base: '/MJScreen/',
   resolve: {
-      alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   css: {
-    postcss: './postcss.config.js', // Assure-toi que le chemin est correct
-  },
-    server: {
-        proxy: {
-            '^/weatherforecast': {
-                target,
-                secure: false
-            }
-        },
-        port: 63396,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
-    }
+    // Si tu utilises PostCSS, assure-toi que ce chemin est correct.
+    postcss: './postcss.config.js'
+  }
 })
