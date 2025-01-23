376f2fd5265cbe1c3695b6adbed3af69aa06e79a
mestileryan <mestileryan@users.noreply.github.com>
2025-01-23 23:12:37 +0000

    deploy: 0f35f95786a15baad037cd954e08a04911c28340

---
+++ assets/paper-plane-C9yftfpT.js
@@ -0,0 +1,1 @@
+import{c as o,a as r,o as t}from"./index-B3h8mBbW.js";const l={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 512 512"};function n(a,e){return t(),o("svg",l,e[0]||(e[0]=[r("path",{fill:"currentColor",d:"M480 40 32 296l112.148 37.383L448 72 209.404 355.135 320 392zM208 376l-16 96 49.932-83.863z"},null,-1)]))}const c={render:n};export{c as default,n as render};
