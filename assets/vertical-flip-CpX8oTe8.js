5683ce29eb46246e8ce4d338b2ec2900a0852ce1
mestileryan <mestileryan@users.noreply.github.com>
2025-02-06 20:56:41 +0000

    deploy: f1873b0b809e9cb315270cb81fd5be156e151892

--- assets/vertical-flip-CpX8oTe8.js
+++
@@ -1,1 +0,0 @@
-import{c as t,a as o,o as r}from"./index-BCvdMMNF.js";const l={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 512 512"};function n(s,e){return r(),t("svg",l,e[0]||(e[0]=[o("path",{fill:"currentColor",d:"M233.373 387.02H165.49L256 477.53l90.51-90.51h-67.883V124.98h67.883L256 34.47l-90.51 90.51h67.883z"},null,-1)]))}const c={render:n};export{c as default,n as render};
