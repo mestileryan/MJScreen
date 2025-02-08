43d9fa69869333075666bb78811c95bd429cdc89
mestileryan <mestileryan@users.noreply.github.com>
2025-02-08 15:01:41 +0000

    deploy: 1aec4c0adc5319f68dfba513b95d819d1f6a0780

---
+++ assets/up-card-DeiB--MB.js
@@ -0,0 +1,1 @@
+import{c as o,a as r,o as t}from"./index-DPaPrL5q.js";const l={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 512 512"};function n(s,e){return t(),o("svg",l,e[0]||(e[0]=[r("path",{fill:"currentColor",d:"M256 47 139.4 202.467l93.6-40.115V359h46V162.352l93.6 40.115zM144 256 32 480h448L368 256h-71v121h-82V256z"},null,-1)]))}const c={render:n};export{c as default,n as render};
