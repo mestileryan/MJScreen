235df745275c2784b00cae158f40bb967ccdf6e5
mestileryan <42813379+mestileryan@users.noreply.github.com>
2025-06-25 19:37:50 +0200

    Ensure settings button works when sidebar collapsed

--- src/components/CollapsibleSidebar.vue
+++ src/components/CollapsibleSidebar.vue
@@ -1,15 +1,19 @@
 <template>
-  <div v-if="!collapsed" class="w-96 bg-gray-800 p-6 flex flex-col justify-start border-l border-gray-700 relative">
-    <button @click="toggle" class="absolute -left-3 top-2 rounded-full p-1 text-purple-300 hover:text-purple-400 hover:bg-gray-700 transition-colors bg-gray-800 border border-gray-600 shadow-md">
-      <ChevronRight class="w-4 h-4" />
+  <div
+    :class="[
+      'relative border-l border-gray-700',
+      collapsed ? 'w-6 flex items-center justify-center' : 'w-96 bg-gray-800 p-6 flex flex-col justify-start'
+    ]"
+  >
+    <button
+      @click="toggle"
+      class="absolute -left-3 top-2 rounded-full p-1 text-purple-300 hover:text-purple-400 hover:bg-gray-700 transition-colors bg-gray-800 border border-gray-600 shadow-md"
+    >
+      <ChevronLeft v-if="collapsed" class="w-4 h-4" />
+      <ChevronRight v-else class="w-4 h-4" />
     </button>
-    <slot />
+    <slot v-if="!collapsed" />
     <slot name="footer" />
-  </div>
-  <div v-else class="relative w-6 flex items-center justify-center border-l border-gray-700">
-    <button @click="toggle" class="absolute -left-3 top-2 rounded-full p-1 text-purple-300 hover:text-purple-400 hover:bg-gray-700 transition-colors bg-gray-800 border border-gray-600 shadow-md">
-      <ChevronLeft class="w-4 h-4" />
-    </button>
   </div>
 </template>
 
